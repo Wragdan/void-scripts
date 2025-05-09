173efe059c05111cc10306e404c1e3514a4e5b70
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-09 04:11:15 -0600

    fix: create boot/efi directory

--- filesystem.sh
+++ filesystem.sh
@@ -17,7 +17,8 @@ umount /mnt
 
 green "Mounting Subvolumes"
 mount -o $BTRFS_OPTS,subvol=@ /dev/mapper/cryptvoid /mnt
-mkdir /mnt/{efi,home,.snapshots}
+mkdir /mnt/{boot,home,.snapshots}
+mkdir /mnt/boot/efi
 mount -o $BTRFS_OPTS,subvol=@home /dev/mapper/cryptvoid /mnt/home
 mount -o $BTRFS_OPTS,subvol=@snapshots /dev/mapper/cryptvoid /mnt/.snapshots
 
