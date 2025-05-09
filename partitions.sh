a7934650342cdb36ba27415ea39ce15bd928c105
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-09 05:37:07 -0600

    chore: delete env file before partitioning

--- partitions.sh
+++ partitions.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+
 red() { echo -e "\033[31m$*\033[0m"; }
 green() { echo -e "\033[32m$*\033[0m"; }
 bold_red() { echo -e "\033[1;31m$*\033[0m"; }
+
+green "Deleting env file"
+rm -rf env.bash
 
 # Check if crypt is already open. Useful for development of the script
 CRYPT_OPEN=$(cryptsetup status cryptvoid)
