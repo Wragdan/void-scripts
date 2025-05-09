e7b4f83d99d3f3cf07b7c2a0d6e8cbd2c2323dd2
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-09 04:32:19 -0600

    chore: start chroot on pre_chroot

--- pre_chroot.sh
+++ pre_chroot.sh
@@ -14,3 +14,16 @@ xbps-install -Sy -R https://repo-default.voidlinux.org/current -r /mnt base-syst
 #green "Generating fstab"
 # Not using xgenfstab since it maps using the mounting point instead of the disk uuid
 #xgenfstab /mnt > /mnt/etc/fstab
+
+green "starting"
+
+#green "Mounting virtual filesystem"
+#for dir in dev proc sys run; do mount --rbind /$dir /mnt/$dir; mount --make-rslave /mnt/$dir; done
+#green "Copying resolve.conf"
+#cp /etc/resolve.conf /mnt/etc/
+#green "Starting chroot"
+#BTRFS_OPTS=$BTRFS_OPTS PS1='(chroot) # ' chroot /mnt/ /bin/bash
+
+# prefer xchroot
+green "Starting chroot"
+BTRFS_OPTS=$BTRFS_OPTS xchroot /mnt
