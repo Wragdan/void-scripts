01ca779d421d58c2494f106954a1d2a8f9896a53
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-09 04:18:00 -0600

    chore: leave filesystem install in partitions script

--- partitions.sh
+++ partitions.sh
@@ -69,3 +69,31 @@ green "Formatting EFI partition"
 mkfs.fat -F32 -n EFI $PART_EFI
 green "Formatting root partition"
 mkfs.btrfs -L Void /dev/mapper/cryptvoid
+
+# Mounting root drive
+BTRFS_OPTS="rw,noatime,compress=zstd,discard=async"
+green "Mounting root partition to '/mnt'"
+mount -o $BTRFS_OPTS /dev/mapper/cryptvoid /mnt
+
+green "Creating SubVolumes"
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@snapshots
+umount /mnt
+
+green "Mounting Subvolumes"
+mount -o $BTRFS_OPTS,subvol=@ /dev/mapper/cryptvoid /mnt
+mkdir /mnt/{boot,home,.snapshots}
+mkdir /mnt/boot/efi
+mount -o $BTRFS_OPTS,subvol=@home /dev/mapper/cryptvoid /mnt/home
+mount -o $BTRFS_OPTS,subvol=@snapshots /dev/mapper/cryptvoid /mnt/.snapshots
+
+# Directories we do not want to snapshot
+green "Creating ignored subvolumes"
+mkdir -p /mnt/var/cache
+btrfs su cr /mnt/var/cache/xbps
+btrfs su cr /mnt/var/tmp
+btrfs su cr /mnt/srv
+
+green "Mounting EFI Partition"
+mount -o rw,noatime $PART_EFI /mnt/boot/efi
