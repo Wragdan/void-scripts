ab1bb0cfa0f4d9a6e34088e93771a264f79e6af0
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-09 03:38:25 -0600

    feat: add subvolumes and required steps before chroot

--- partitions.sh
+++ partitions.sh
@@ -70,3 +70,38 @@ mkfs.fat -F32 -n EFI $PART_EFI
 green "Formatting root partition"
 mkfs.btrfs -L Void /dev/mapper/cryptvoid
 
+
+
+# Mounting root drive
+BTRFS_OPTS="rw,noatime,compress=zstd,discard=async"
+green "Mounting root partition to '/mnt'"
+mount -o $BTRFS_OPTS /dev/mapper/cryptvoid /mnt
+
+green "Creating SubVolumes"
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@snapshots
+umount /mnt
+
+green "Mounting Subvolumes"
+mount -o $BTRFS_OPTS,subvol=@ /dev/mapper/cryptvoid /mnt
+mkdir /mnt/{efi,home,.snapshots}
+mount -o $BTRFS_OPTS,subvol=@home /dev/mapper/cryptvoid /mnt/home
+mount -o $BTRFS_OPTS,subvol=@snapshots /dev/mapper/cryptvoid /mnt/.snapshots
+
+# Directories we do not want to snapshot
+green "Creating ignored subvolumes"
+mkdir -p /mnt/var/cache
+btrfs su cr /mnt/var/cache/xbps
+btrfs su cr /mnt/var/tmp
+btrfs su cr /mnt/srv
+
+green "Mounting EFI Partition"
+mount -o rw,noatime $PART_EFI /mnt/boot/efi
+
+green "Copying xbps keys"
+mkdir -p /mnt/var/db/xbps/keys
+cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
+
+green "Installing base system and required packages"
+xbps-install -Sy -R https://repo-default.voidlinux.org/current -r /mnt base-system cryptsetup grub-x86_64-efi btrfs-progs vim
