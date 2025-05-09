d6215f3cf5c2305c7a8eaa729b2b856c0ad7ca63
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-09 01:47:09 -0600

    chore: detect partitions

--- partitions.sh
+++ partitions.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "Enter drive to use for installation"
+echo "Enter drive to use for installation without the '/dev/'. Ex. nvme0n1, sda, sdb, etc..."
 read DRIVE
 
-echo "This script will delete all partitions, write 'yes' to continue"
-read DELETE_PARTITIONS
+FULL_DRIVE="/dev/$DRIVE"
 
-if [[ "$DELETE_PARTITIONS" != "yes" ]]; then
+echo "This script will delete all partitions, write 'yes' to continue"
+read ANSWER
+
+if [[ "$ANSWER" != "yes" ]]; then
     echo "You decided to cancel"
     exit 0
 fi
@@ -14,9 +16,29 @@ fi
 # Deletes all partitions on $DRIVE
 echo "Deleting all partitions on disk"
 echo "..."
-sfdisk --delete $DRIVE
+sfdisk --delete $FULL_DRIVE
 
 # Creates 2 partitions, EFI 256M and Linux for the remaining of the disk
 echo "Creating partitions"
 echo "..."
-echo -e 'size=256M, type=U\n size=+, type=L\n' | sfdisk $DRIVE -W always
+echo -e 'size=256M, type=U\n size=+, type=L\n' | sfdisk $FULL_DRIVE -W always
+
+PART_EFI=
+PART_LINUX=
+
+if [[ $FULL_DRIVE == *nvme* ]]; then
+    echo "Detected NVME"
+    PART_EFI="${FULL_DRIVE}p1"
+    PART_LINUX="${FULL_DRIVE}p2"
+fi
+
+if [[ $FULL_DRIVE == */sd* ]]; then
+    echo "Detedted HDD"
+    PART_EFI="${FULL_DRIVE}1"
+    PART_LINUX="${FULL_DRIVE}2"
+fi
+
+if [[ -z "$PART_EFI" ]]; then
+    echo "Could not detect efi partition. aborting..."
+    exit 1
+fi
