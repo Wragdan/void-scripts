ce33995f87072f3f2c98ff7a6586e81fc13588ac
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-09 01:13:24 -0600

    fix: partition delete prompt

--- partitions.sh
+++ partitions.sh
@@ -3,6 +3,13 @@
 echo "Enter drive to use for installation"
 read DRIVE
 
-echo "This script will delete all partitions, if you want to cancel press (ctrl + c)"
+echo "This script will delete all partitions, write 'yes' to continue"
+read DELETE_PARTITIONS
 
+if [[ "$DELETE_PARTITIONS" != "yes" ]]; then
+    echo "You decided to cancel"
+    exit 0
+fi
+
+# Deletes all partitions on $DRIVE
 sfdisk --delete $DRIVE
