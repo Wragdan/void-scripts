80b66874e3d789da3cac0bd0388e5a556cda98eb
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-09 01:10:13 -0600

    feat: remove all partitions before installing

--- partitions.sh
+++ partitions.sh
@@ -3,4 +3,6 @@
 echo "Enter drive to use for installation"
 read DRIVE
 
-echo "You selected $DRIVE"
+echo "This script will delete all partitions, if you want to cancel press (ctrl + c)"
+
+sfdisk --delete $DRIVE
