44c2e42e8ebcc6dce179f2a0bf309bb30bafe546
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-09 02:06:13 -0600

    feat: color logs

--- partitions.sh
+++ partitions.sh
@@ -1,49 +1,65 @@
 #!/bin/bash
 
+red() { echo -e "\033[31m$*\033[0m"; }
+green() { echo -e "\033[32m$*\033[0m"; }
+bold_red() { echo -e "\033[1;31m$*\033[0m"; }
+
+# Use the functions
+#red "Error occurred!"
+#green "Operation successful!"
+#bold_red "CRITICAL ERROR!"
+
+# Check if crypt is already open. Useful for development of the script
+CRYPT_OPEN=$(cryptsetup status cryptvoid)
+
+if [[ $CRYPT_OPEN == *is active* ]]; then
+    red "Crypt is open, closing..."
+    cryptsetup close cryptvoid
+fi
+
 echo "Enter drive to use for installation without the '/dev/'. Ex. nvme0n1, sda, sdb, etc..."
 read DRIVE
 
 FULL_DRIVE="/dev/$DRIVE"
 
-echo "This script will delete all partitions, write 'yes' to continue"
+red "WARNING!!! This script will delete all partitions, write 'yes' to continue"
 read ANSWER
 
 if [[ "$ANSWER" != "yes" ]]; then
-    echo "You decided to cancel"
+    bold_red "You decided to cancel"
     exit 0
 fi
 
 # Deletes all partitions on $DRIVE
-echo "Deleting all partitions on disk"
-echo "..."
+green "Deleting all partitions on disk"
 sfdisk --delete $FULL_DRIVE
 
 # Creates 2 partitions, EFI 256M and Linux for the remaining of the disk
-echo "Creating partitions"
-echo "..."
+green "Creating partitions"
 echo -e 'size=256M, type=U\n size=+, type=L\n' | sfdisk $FULL_DRIVE -W always
 
 PART_EFI=
 PART_LINUX=
 
 if [[ $FULL_DRIVE == *nvme* ]]; then
-    echo "Detected NVME"
+    green "Detected NVME"
     PART_EFI="${FULL_DRIVE}p1"
     PART_LINUX="${FULL_DRIVE}p2"
 fi
 
 if [[ $FULL_DRIVE == */sd* ]]; then
-    echo "Detedted HDD"
+    green "Detedted HDD"
     PART_EFI="${FULL_DRIVE}1"
     PART_LINUX="${FULL_DRIVE}2"
 fi
 
 if [[ -z "$PART_EFI" ]]; then
-    echo "Could not detect efi partition. aborting..."
+    bold_red "Could not detect efi partition. aborting..."
     exit 1
 fi
 
-echo "Encrypting Linux partition"
-echo "Please complete the installation by providing the necessary inputs"
+
+green "Encrypting Linux partition"
 cryptsetup luksFormat --type luks1 -y $PART_LINUX
+green "Opening encrypted partition"
 cryptsetup luksOpen $PART_LINUX cryptvoid
