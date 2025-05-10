48b43dfc069f159714e701d3afde8c24fc868d92
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-09 23:29:50 -0600

    chore: use static path for git clone

--- user.sh
+++ user.sh
@@ -5,8 +5,8 @@ green() { echo -e "\033[32m$*\033[0m"; }
 bold_red() { echo -e "\033[1;31m$*\033[0m"; }
 
 green "Cloning personal void-packages"
-git clone https://github.com/Wragdan/void-packages
-cd void-packages
+git clone https://github.com/Wragdan/void-packages /home/wragdan/void-packages
+cd /home/wragdan/void-packages
 green "Checking out to branch wragdan"
 git checkout wragdan
 
