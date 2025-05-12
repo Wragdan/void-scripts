ad87fad48ba3ed53a889aa9e73758d1dd69c8383
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-12 08:22:01 -0600

    feat: add env script for common environment config

---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+red() { echo -e "\033[31m$*\033[0m"; }
+green() { echo -e "\033[32m$*\033[0m"; }
+bold_red() { echo -e "\033[1;31m$*\033[0m"; }
+
+green "Setting default shell for wragdan to zsh"
+chsh -s /usr/bin/zsh
