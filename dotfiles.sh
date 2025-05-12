4652797068bd4609b38b90d98fbf088531c5ba12
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-05-12 08:18:01 -0600

    fix: temporary fix for dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -6,6 +6,7 @@ bold_red() { echo -e "\033[1;31m$*\033[0m"; }
 
 git clone https://github.com/Wragdan/dotfiles /home/wragdan/.dotfiles
 cd /home/wragdan/.dotfiles
+git checkout voidlinux
 
 stow x11
 stow shell
