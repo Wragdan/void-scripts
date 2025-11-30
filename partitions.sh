baba91c582d99f35fa115fab9fae90a78f502846
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-11-30 02:22:30 -0600

    chore: testing some improvements to dialog

--- partitions.sh
+++ partitions.sh
@@ -2,8 +2,7 @@
 
 #set -e
 
-TITLE="Void Linux Installer"
-BACKTITLE="Partition Disks"
+BACKTITLE="Void Linux Installer - Partitioning"
 
 if ! command -v dialog &> /dev/null; then
     echo "Error: 'dialog' command not found. Please install it (e.g., 'sudo xbps-install dialog' on Void)."
@@ -11,14 +10,14 @@ if ! command -v dialog &> /dev/null; then
 fi
 
 dialog --backtitle "$BACKTITLE" \
-       --title "$TITLE" \
+       --title "Initial cleanup" \
        --msgbox "Starting cleanup. Deleting 'env.bash' file." 6 50
 
 echo "Deleting env file"
 rm -rf env.bash
 
 dialog --backtitle "$BACKTITLE" \
-       --title "$TITLE" \
+       --title "Initial cleanup" \
        --msgbox "Unmounting file systems..." 6 50
 echo "Unmounting partitions..."
 umount /mnt/.snapshots 2>/dev/null
@@ -80,11 +79,11 @@ FULL_DRIVE="/dev/$DRIVE"
 echo "export FULL_DRIVE=$FULL_DRIVE" >> env.bash
 
 dialog --backtitle "$BACKTITLE" \
-       --title "$TITLE" \
+       --title "WARNING" \
        --msgbox "!!! WARNING !!!\n\nThis next step will irrevocably **ERASE ALL DATA** and **DELETE ALL PARTITIONS** on the drive: **$FULL_DRIVE**.\n\nPress OK to proceed to the confirmation." 12 70
 
 ANSWER=$(dialog --backtitle "$BACKTITLE" \
-                --title "$TITLE - CONFIRM DELETION" \
+                --title "CONFIRM DELETION" \
                 --inputbox "To continue and delete all data on $FULL_DRIVE, type **yes** below:" 10 60 2>&1 >/dev/tty)
 
 # Check if the user pressed Cancel or provided empty input
@@ -99,53 +98,46 @@ else
     dialog --backtitle "$BACKTITLE" \
            --title "Action Canceled" \
            --msgbox "You decided to cancel the operation.\nExiting the installer." 8 50
-    echo "You decided to cancel"
-    exit 0 # Exit the script
+    exit 0
 fi
 
-dialog --backtitle "$BACKTITLE" \
-       --title "Partitioning" \
-       --msgbox "Deleting all partitions on $FULL_DRIVE..." 8 60
+(
+    echo "Deleting all partitions on $FULL_DRIVE..."
+    sfdisk --delete "$FULL_DRIVE" -W always 2>/dev/null
+    echo "Partitions deleted. Creating new partitions..."
+    # Creates 2 partitions, EFI 256M and Linux for the remaining of the disk
+    echo -e 'size=256M, type=U\n size=+, type=L\n' | sfdisk $FULL_DRIVE -W always
 
-echo "Deleting all partitions on disk"
-sfdisk --delete "$FULL_DRIVE" -W always
+    PART_EFI=
+    PART_LINUX=
 
-dialog --backtitle "$BACKTITLE" \
-       --title "Partitioning" \
-       --msgbox "All partitions on $FULL_DRIVE have been successfully deleted. Proceeding to create new partitions." 8 60
+    if [[ $FULL_DRIVE == *nvme* ]]; then
+        echo "Detected NVME"
+        PART_EFI="${FULL_DRIVE}p1"
+        PART_LINUX="${FULL_DRIVE}p2"
+    fi
 
-## Creates 2 partitions, EFI 256M and Linux for the remaining of the disk
-#green "Creating partitions"
-#echo -e 'size=256M, type=U\n size=+, type=L\n' | sfdisk $FULL_DRIVE -W always
-#
-#PART_EFI=
-#PART_LINUX=
-#
-#if [[ $FULL_DRIVE == *nvme* ]]; then
-#    green "Detected NVME"
-#    PART_EFI="${FULL_DRIVE}p1"
-#    PART_LINUX="${FULL_DRIVE}p2"
-#fi
-#
-#if [[ $FULL_DRIVE == */sd* ]]; then
-#    green "Detedted HDD"
-#    PART_EFI="${FULL_DRIVE}1"
-#    PART_LINUX="${FULL_DRIVE}2"
-#fi
-#
-#if [[ -z "$PART_EFI" ]]; then
-#    bold_red "Could not detect efi partition. aborting..."
-#    exit 1
-#fi
-#
-#echo "export PART_EFI=$PART_EFI" >> env.bash
-#echo "export PART_LINUX=$PART_LINUX" >> env.bash
-#
-#green "Encrypting Linux partition"
-#cryptsetup luksFormat --type luks1 -y $PART_LINUX
-#green "Opening encrypted partition"
-#cryptsetup luksOpen $PART_LINUX cryptvoid
-#
+    if [[ $FULL_DRIVE == */sd* ]]; then
+        echo "Detected HDD"
+        PART_EFI="${FULL_DRIVE}1"
+        PART_LINUX="${FULL_DRIVE}2"
+    fi
+
+    if [[ -z "$PART_EFI" ]]; then
+        echo "Could not detect efi partition. aborting..."
+        exit 1
+    fi
+
+    echo "export PART_EFI=$PART_EFI" >> env.bash
+    echo "export PART_LINUX=$PART_LINUX" >> env.bash
+
+    echo "Encrypting Linux partition"
+    cryptsetup luksFormat --type luks1 -y $PART_LINUX
+    cryptsetup luksOpen $PART_LINUX cryptvoid
+    sleep 3
+    echo "Completed partitioning."
+) 2>&1 | dialog --title "$TITLE" --progressbox 15 70
+
 #green "Formatting partitions"
 #green "Formatting EFI partition"
 #mkfs.fat -F32 -n EFI $PART_EFI
