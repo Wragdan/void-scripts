e708c2771a17a7ffa8181255b835e8bfb65134fd
Wragdan <17775924+Wragdan@users.noreply.github.com>
2025-12-01 00:34:06 -0600

    chore: change pre_chroot to use dialog

--- pre_chroot.sh
+++ pre_chroot.sh
@@ -2,33 +2,23 @@
 
 source env.bash
 
-red() { echo -e "\033[31m$*\033[0m"; }
-green() { echo -e "\033[32m$*\033[0m"; }
-bold_red() { echo -e "\033[1;31m$*\033[0m"; }
 
-green "Copying xbps keys"
-mkdir -p /mnt/var/db/xbps/keys
-cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
+(
+    echo "Copying xbps keys"
+    mkdir -p /mnt/var/db/xbps/keys
+    cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
 
-green "Installing base system and required packages"
-xbps-install -Sy -R https://repo-default.voidlinux.org/current -r /mnt base-system cryptsetup grub-x86_64-efi btrfs-progs vim
+    echo "Installing base system and required packages"
+    xbps-install -Sy -R https://repo-default.voidlinux.org/current -r /mnt base-system cryptsetup grub-x86_64-efi btrfs-progs
 
-#green "Generating fstab"
-# Not using xgenfstab since it maps using the mounting point instead of the disk uuid
-#xgenfstab /mnt > /mnt/etc/fstab
+    echo "Copying void scripts to /mnt"
+    cp -r ../void-scripts /mnt/tmp/void-scripts
 
-green "Copying void scripts to /mnt"
-cp -r ../void-scripts /mnt/tmp/void-scripts
+) 2>&1 | dialog --title "$TITLE" --progressbox 30 120
 
-#green "Mounting virtual filesystem"
-#for dir in dev proc sys run; do mount --rbind /$dir /mnt/$dir; mount --make-rslave /mnt/$dir; done
-#green "Copying resolve.conf"
-#cp /etc/resolve.conf /mnt/etc/
-#green "Starting chroot"
-#BTRFS_OPTS=$BTRFS_OPTS PS1='(chroot) # ' chroot /mnt/ /bin/bash
 
-green "After chroot starts, run /tmp/void-scripts/system.sh"
+dialog --backtitle "$BACKTITLE" \
+       --title "Chroot prepared" \
+       --msgbox "After chroot starts, run /tmp/void-scripts/system.sh" 8 50
 
-# prefer xchroot
-green "Starting chroot"
 xchroot /mnt
